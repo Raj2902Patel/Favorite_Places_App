183aa0011bdd5b704ebe0c12dc7025f73b59ed2c
Raj Patel <rajpatel.imscit20@gmail.com>
2023-11-29 14:56:36 +0530

    Some Changes

--- favorite_places_app/lib/widgets/image_input.dart
+++ favorite_places_app/lib/widgets/image_input.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  const ImageInput({super.key, required this.onPickImage});
+
+  final void Function(File image) onPickImage;
 
   @override
   State<ImageInput> createState() {
@@ -10,28 +14,53 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
 
-  void _takePicture() {
+    if (pickedImage == null) {
+      return;
+    }
 
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+
+    widget.onPickImage(_selectedImage!);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-        ),
-      ),
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-        onPressed: _takePicture,
-      ),
+    Widget content = TextButton.icon(
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+      onPressed: _takePicture,
     );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+          ),
+        ),
+        height: 250,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: content);
   }
 }
